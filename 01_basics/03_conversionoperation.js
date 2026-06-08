158821a5d117ad19f203ad584e35ae6e9014f118
DYNAMIC-CDS <work.diwakar06@gmail.com>
2026-06-08 11:14:43 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,19 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+
+// "33"=>33
+//"33abc"=> NAN
+// true=>1; false=>0
+
+let isLoggedIn = 1
+let bolleanIsLoggedIn = Boolean(isLoggedIn);//true
+//""=> false
+//"hehe"=> true
+// 0  gives true and 1 gives false
+
+//can be converted to string ddd
