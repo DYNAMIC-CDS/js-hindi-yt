06ef8ea67329b4eec47157ff12c0eea130907ab5
DYNAMIC-CDS <work.diwakar06@gmail.com>
2026-06-08 11:00:15 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 3245454
+let accountEmail = "hehe@google.com"
+var accountpassword = "12345"
+accountcity = "jaipur"
+let accountState
+
+//accountId = 2//not allowed
+
+console.log(accountId);
+accountEmail = "hihi@google.com"
+accountpassword = "212121"
+accountCity = "bengaluru"
+/*
+dont use var bcoz of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountpassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version 
+
+//alert(3+3) we are using node js not browser
+
+console.log(3+3) // code readability should be high 
+
+console.log("hehe daddy")
+
+let name = "hehe"
+let age= 69
+let isloggedin = false
+
+// number => 2^53
+//bigint
+//string => ""
+// null => standallone value
+//undefined => 
+    //symbol=> unique
+//object
+
+
+console.log (typeof age);
+
+console.log (typeof "hitesh");
+console.log (typeof hitesh);
+console.log (typeof null);
+console.log (typeof undefined);
